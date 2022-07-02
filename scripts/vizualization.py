6a1b163f7c513fecb7e12168bbfca4fdfbf9b697
Micky373 <michaeltamirie@gmail.com>
2022-07-02 15:12:58 +0300

    Create vizualization script and count_plot module

---
+++ scripts/vizualization.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def count_plot(df:pd.DataFrame, column:str) -> None:
+    plt.figure(figsize=(10, 8))
+    sns.countplot(data=df, x=column)
+    plt.xlabel(f'{column}', fontsize=17)
+    plt.ylabel("Count", fontsize=17)
+    plt.title(f'\nDistribution of {column}\n', size=20, fontweight='bold')
+    plt.savefig('../charts/count_plot.jpg')
+    plt.show()
